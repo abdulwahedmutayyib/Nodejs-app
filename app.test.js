d2799f08781db16b99942221877bca88cefbda0f
wahed <wahedmutayyib@gmail.com>
2024-12-18 20:54:20 +0530

    dockerfile

---
+++ app.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('GET /', () => {
+  it('should respond with 200 and a message', async () => {
+    const response = await request(app).get('/');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello from Node.js!');
+  });
+});
